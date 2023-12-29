901228bcd7663a1a3d18305415c6d8b63aab8d11
Kushan Thamel <kushan.thamel@gmail.com>
2023-12-29 10:37:33 +0000

    fstab content update config was added

--- script.sh
+++ script.sh
@@ -10,6 +10,9 @@ echo "## Attach the volume ##"
 sudo mkfs.ext4 -F /dev/xvdk
 sudo mount /dev/xvdk /mnt
 sudo mkdir /mnt/vault-data
+BLK_ID=$(sudo blkid /dev/xvdk | cut -f2 -d" ")
+echo "$BLK_ID     /mnt   ext4    defaults   0   2" | sudo tee --append /etc/fstab
+sudo mount -a
 
 echo "## Create user for vault service ##"
 sudo useradd -m -d /mnt/vault-data vault
